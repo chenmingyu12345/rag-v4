f70c0988769ed8a568bc272892ac1b30bc12956f
chenmingyu12345 <chenmingyu12345@gmail.com>
2026-04-03 12:02:00 +0800

    Add eval_dataset.py

---
+++ eval_dataset.py
@@ -0,0 +1,22 @@
+dataset = [
+    {
+        "query": "What is artificial intelligence?",
+        "relevant_doc": "Artificial Intelligence simulates human intelligence."
+    },
+    {
+        "query": "What is machine learning?",
+        "relevant_doc": "Machine learning is a subset of AI."
+    },
+    {
+        "query": "What is deep learning?",
+        "relevant_doc": "Deep learning is a subset of machine learning."
+    },
+    {
+        "query": "What is natural language processing?",
+        "relevant_doc": "Natural language processing is a field of AI."
+    },
+    {
+        "query": "What is computer vision?",
+        "relevant_doc": "Computer vision is a field of AI."
+    }
+]
